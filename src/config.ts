1f755d4c41a4c3664c91b03cc69186c59e9ddac9
Aadi Poddar <aadipoddarmail@gmail.com>
2023-09-08 16:44:57 +0530

    Update Website Info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog Posts';
+export const SITE_DESCRIPTION = 'A Blog Post Website using Astro';
+export const HOMEPAFE_URL = 'https://www.blogaadi.vercel.app'
